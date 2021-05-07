0db9d7a9702ccef56ddcdec27564f1775d35c529
Rajeev <s2100155@edu.bc.fi>
2021-05-07 11:33:16 +0300

    add file

--- src/Controller/HomeScreenController.php
+++ src/Controller/HomeScreenController.php
@@ -4,22 +4,55 @@
 namespace App\Controller;
 
 
+use App\Entity\Recipe;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeScreenController extends AbstractController
 {
+
+
     /**
-     * @Route("/homeScreen", methods={"GET", "DELETE"})
+     * @Route("/newrecipe/add", name="add_new_recipe")
      */
-public function home() {
-  /* $resp = new Response('<h1>hello world</h1>');
-    return $resp;*/
+    public function addRecipe(){
+        $entityManager = $this->getDoctrine()->getManager();
 
-    return $this->json(['message' => 'hello world']);
+        $newRecipe = new Recipe();
+        $newRecipe->setName('Omlet');
+        $newRecipe->setIngredients('Omlet, oil');
+        $newRecipe->setDifficulty('easy');
+
+        $newRecipe1 = new Recipe();
+        $newRecipe1->setName('coffee');
+        $newRecipe1->setIngredients('coffee, milk, sugar');
+        $newRecipe1->setDifficulty('very easy');
 
 
-}
+        $entityManager->persist($newRecipe);
+        $entityManager->persist($newRecipe1);
+        $entityManager->flush();
+
+    return new Response('try to add ....' . $newRecipe1->getId() . " " . $newRecipe->getId());
+    }
+
+    /**
+     * @Route("/newrecipe/all", name="get_all_recipe")
+     */
+    public function getAllRecipe() {
+        $recipes = $this->getDoctrine()->getRepository(Recipe::class)->findAll();
+        $response = [];
+
+        foreach ($recipes as $recipe) {
+            $response[] = array (
+                'name' => $recipe->getName(),
+                'ingredients' => $recipe->getIngredients(),
+                'difficulty' => $recipe->getDifficulty()
+
+            );
+        }
+        return $this->json($response);
+    }
 
 }
